38a44f42ee02100dd0b8938dfe448e33bca006c5
Dan Toose <dantoose@gmail.com>
2022-07-13 15:16:32 +1000

    Added Gap Prefab for Plank use
    This includes scripts and the entire plank&gap prefab. By default it uses the 1 key for placing planks.

---
+++ Assets/Scripts/UsePlank.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsePlank : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject plankObject;
+    public GameObject blockObject;
+    private Collider blockMesh;
+    private MeshRenderer plankMesh;
+    private bool isPlankPlaced;
+    private bool plankIsPlacable;
+    private bool keyWasPressed;
+    public KeyCode keyToPlacePlank;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockMesh = blockObject.GetComponent<Collider>();
+        plankMesh = plankObject.GetComponent<MeshRenderer>();
+        player = GameObject.FindWithTag("Player");
+        plankMesh.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyWasPressed = false;
+        keyWasPressed = Input.GetKeyDown(keyToPlacePlank);
+
+        if (plankIsPlacable == true && keyWasPressed)
+        {
+            Debug.Log("plank placement fired");
+            plankIsPlacable = false;
+            blockMesh.GetComponent<Collider>().enabled = false;
+            plankMesh.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isPlankPlaced == false)
+        {
+            if (player.GetComponent<PlayerInventory>().plankCount > 0)
+            {
+                plankIsPlacable = true;
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isPlankPlaced == true)
+        {
+            plankIsPlacable = false;
+        }
+    }
+}
