3d8227bb83c7d9e5cc2342953e74e2cc102b3b30
Dan Toose <dantoose@gmail.com>
2022-07-15 15:46:37 +1000

    Added death panel
    Clearer as to what happens on death

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -14,14 +14,30 @@ public class PlayerHealth : MonoBehaviour
 
     private bool keyWasPressed;
 
+    public Text deathText;
+    public Text respawnText;
+    public Image deathPanel;
+
     // THIS SCRIPT ALSO CONTAINS RESPAWN INFO FOR THE PLAYER
     //public GameObject currentCheckpoint;
     public float respawnDelay = 3.0f;
     public Respawner respawn;
 
+    public bool playerIsAlive;
+    private bool readyToRespawn;
+
     // Start is called before the first frame update
     void Start()
     {
+        deathText = GameObject.Find("DeathText").GetComponent<Text>();
+        respawnText = GameObject.Find("RespawnText").GetComponent<Text>();
+        deathPanel = GameObject.Find("DeathPanel").GetComponent<Image>();
+
+        playerIsAlive = true;
+        respawnText.text = "";
+        deathText.text = "";
+        deathPanel.gameObject.SetActive(false);
+
         player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = playerMaxHealth;
         healthText.text = "Health: " + playerHealth;
@@ -54,19 +70,36 @@ public class PlayerHealth : MonoBehaviour
                 playerHealth = playerMaxHealth;
             }
         }
+
+        if (readyToRespawn && Input.anyKeyDown)
+        {
+            readyToRespawn = false;
+            respawnText.text = "";
+            deathText.text = "";
+            deathPanel.gameObject.SetActive(false);
+            ActualRespawn();
+        }
     }
 
     public void playerDeath()
     {
         // death stuff
-        // Set a delay
+        playerIsAlive = false;
+        deathPanel.gameObject.SetActive(true);
+        deathText.text = "You Died!";
         Invoke("RespawnFromDeath", respawnDelay);
-
     }
 
     void RespawnFromDeath()
     {
-        respawn.RespawnPlayer();
+        respawnText.text = "Press any key to respawn";
+        readyToRespawn = true;
+    }
+
+    void ActualRespawn()
+    {
         playerHealth = playerMaxHealth;
+        playerIsAlive = true;
+        respawn.RespawnPlayer();
     }
 }
